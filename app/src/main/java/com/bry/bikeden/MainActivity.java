02a7f35fe2070c49b205a117e3d25912f444fa84
bryonyoni <bryonyoni@gmail.com>
2017-05-26 16:11:48 +0300

    added images and a button to the app.

--- app/src/main/java/com/bry/bikeden/MainActivity.java
+++ app/src/main/java/com/bry/bikeden/MainActivity.java
@@ -1,13 +1,23 @@
 package com.bry.bikeden;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.appNameTextView)  TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Typeface ostrichFont = Typeface.createFromAsset(getAssets(),"fonts/ostrichRegular.ttf");
+        mAppNameTextView.setTypeface(ostrichFont);
     }
 }
