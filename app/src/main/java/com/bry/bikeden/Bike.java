628fae83cd19864c1d74cbfc4775b6997949530f
bryonyoni <bryonyoni@gmail.com>
2017-05-26 17:50:23 +0300

    created a bike activity

---
+++ app/src/main/java/com/bry/bikeden/Bike.java
@@ -0,0 +1,13 @@
+package com.bry.bikeden;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Bike extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bike);
+    }
+}
--- app/src/main/java/com/bry/bikeden/MainActivity.java
+++ app/src/main/java/com/bry/bikeden/MainActivity.java
@@ -1,8 +1,11 @@
 package com.bry.bikeden;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import butterknife.Bind;
@@ -10,6 +13,7 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     @Bind(R.id.appNameTextView)  TextView mAppNameTextView;
+    @Bind(R.id.bikesactivitybutton)  Button mBikesActivitybutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +23,18 @@ public class MainActivity extends AppCompatActivity {
 
         Typeface ostrichFont = Typeface.createFromAsset(getAssets(),"fonts/ostrichRegular.ttf");
         mAppNameTextView.setTypeface(ostrichFont);
+
+        mBikesActivitybutton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, Bike.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
+
+
 }
