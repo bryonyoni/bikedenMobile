312634c6c6264bc807e8fe21c08abfc952f550c8
bryonyoni <bryonyoni@gmail.com>
2017-06-09 11:31:26 +0300

    Adde a login and sign up activity.
    Added the login and sign up activities for users to login and sign up.

--- app/src/main/java/com/bry/bikeden/Bike.java
+++ app/src/main/java/com/bry/bikeden/Bike.java
@@ -1,13 +1,32 @@
 package com.bry.bikeden;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class Bike extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class Bike extends AppCompatActivity implements View.OnClickListener{
+//    @Bind(R.id.bikeImageView) ImageView mImageLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ButterKnife.bind(this);
         setContentView(R.layout.activity_bike);
+
+//        mImageLabel.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v){
+//        if(v == mImageLabel){
+//          Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("Harley Davidson"));
+//        }
     }
 }
